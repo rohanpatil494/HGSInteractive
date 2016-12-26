3fcd10afd33961c659f60a8b5630453eafc696ba
rohanpatil494 <rohan@htmtinteractive.com>
2016-12-26 12:33:34 +0530

    dfsdf asides

--- HGSInteractive/BaseViewController.swift
+++ HGSInteractive/BaseViewController.swift
@@ -13,7 +13,7 @@ class BaseViewController: UIViewController, SlideMenuDelegate  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        ////////
         // Do any additional setup after loading the view.
     }
     
